1ce166888eec4a0949b2af05ea458d8122fa6ee0
Rui Coelho <ruicoelhopedro@gmail.com>
2021-09-27 19:52:19 +0100

    Only extend captures with good history

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,8 @@ moves_loop: // When in check, search starts here
       // Capture extensions for PvNodes and cutNodes
       else if (   (PvNode || cutNode)
                && captureOrPromotion
-               && moveCount != 1)
+               && moveCount != 1
+               && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] > 0)
           extension = 1;
 
       // Check extensions
