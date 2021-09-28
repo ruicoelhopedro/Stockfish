7c514047626039f7a8ec3759d0f2b2088a516196
Rui Coelho <ruicoelhopedro@gmail.com>
2021-09-28 13:45:48 +0100

    Reduced futility move count for Pv nodes

--- src/search.cpp
+++ src/search.cpp
@@ -74,8 +74,8 @@ namespace {
     return (r + 534) / 1024 + (!i && r > 904) + rangeReduction;
   }
 
-  constexpr int futility_move_count(bool improving, Depth depth) {
-    return (3 + depth * depth) / (2 - improving);
+  constexpr int futility_move_count(bool improving, Depth depth, bool PvNode) {
+    return (3 + depth * depth) / (2 - improving) + 5 * PvNode;
   }
 
   // History and stats update bonus, based on depth
@@ -1039,7 +1039,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth, PvNode);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, rangeReduction > 2), 0);
