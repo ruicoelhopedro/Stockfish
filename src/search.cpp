4061466bb3a1cc4a7a8af742237b9b9fd6dbb30a
Rui Coelho <ruicoelhopedro@gmail.com>
2021-10-29 19:30:33 +0100

    Increase lmrDepth in PV nodes

--- src/search.cpp
+++ src/search.cpp
@@ -1033,7 +1033,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, rangeReduction > 2), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, rangeReduction > 2) + PvNode, 0);
 
           if (   captureOrPromotion
               || givesCheck)
