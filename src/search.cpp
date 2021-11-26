554334a923de12b3bf74fa24607b9c829dee8649
Rui Coelho <ruicoelhopedro@gmail.com>
2021-11-26 17:19:43 +0000

    Futility margin on evalDepth

--- src/search.cpp
+++ src/search.cpp
@@ -62,8 +62,8 @@ namespace {
   enum NodeType { NonPV, PV, Root };
 
   // Futility margin
-  Value futility_margin(Depth d, bool improving) {
-    return Value(214 * (d - improving));
+  Value futility_margin(Depth d, bool improving, Depth evalDepth) {
+    return Value(214 * (d - improving) - 50 * evalDepth);
   }
 
   // Reductions lookup table, initialized at startup
@@ -823,7 +823,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
-        &&  eval - futility_margin(depth, improving) >= beta
+        &&  eval - futility_margin(depth, improving, evalDepth) >= beta
         &&  eval < 15000) // 50% larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;
 
