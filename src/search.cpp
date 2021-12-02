17bda757b084aed6abd5edb8e721c6e7d1814c27
Rui Coelho <ruicoelhopedro@gmail.com>
2021-12-02 16:12:20 +0000

    Multi-cut reduce instead of prune

--- src/search.cpp
+++ src/search.cpp
@@ -1118,7 +1118,7 @@ moves_loop: // When in check, search starts here
           // that multiple moves fail high, and we can prune the whole subtree by returning
           // a soft bound.
           else if (singularBeta >= beta)
-              return singularBeta;
+              extension = 3 - depth;
 
           // If the eval of ttMove is greater than beta, we reduce it (negative extension)
           else if (ttValue >= beta)
