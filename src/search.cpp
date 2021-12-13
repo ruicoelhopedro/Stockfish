f28205ec8dbfaa995939fc2ddafaaa3f862e7ce4
Rui Coelho <ruicoelhopedro@gmail.com>
2021-12-13 19:08:42 +0000

    Null move on pvDistance

--- src/search.cpp
+++ src/search.cpp
@@ -826,7 +826,7 @@ namespace {
         return eval;
 
     // Step 8. Null move search with verification search (~40 Elo)
-    if (   !PvNode
+    if (   ss->pvDistance > 1
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 23767
         &&  eval >= beta
