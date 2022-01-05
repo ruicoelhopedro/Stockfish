86faadff9af53b44e0ee10e75f9aa7096db26c19
Rui Coelho <ruicoelhopedro@gmail.com>
2022-01-05 18:05:10 +0000

    Take 1

--- src/search.cpp
+++ src/search.cpp
@@ -1125,7 +1125,7 @@ moves_loop: // When in check, search starts here
       else if (   PvNode
                && move == ttMove
                && move == ss->killers[0]
-               && (*contHist[0])[movedPiece][to_sq(move)] >= 10000)
+               && ((*contHist[0])[movedPiece][to_sq(move)] >= 10000 || (ss-1)->moveCount > 5))
           extension = 1;
 
       // Add extension to new depth
