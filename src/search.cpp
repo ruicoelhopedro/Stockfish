af95241a7339078814ea28453fd0042575cf34b2
Rui Coelho <ruicoelhopedro@gmail.com>
2022-01-10 18:20:30 +0000

    No non-quiet SEE pruning for PvNodes

--- src/search.cpp
+++ src/search.cpp
@@ -1040,7 +1040,8 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning (~9 Elo)
-              if (!pos.see_ge(move, Value(-218) * depth))
+              if (   !PvNode
+                  && !pos.see_ge(move, Value(-218) * depth))
                   continue;
           }
           else
