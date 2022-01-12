36cd7267d6cd3f29a3db7e04cb817f0eb873dc4b
Rui Coelho <ruicoelhopedro@gmail.com>
2022-01-12 14:54:38 +0000

    Take 5

--- src/search.cpp
+++ src/search.cpp
@@ -820,7 +820,7 @@ namespace {
         && (ss-1)->statScore < 23767
         &&  eval >= beta
         &&  eval >= ss->staticEval
-        &&  ss->staticEval >= beta - 20 * depth - improvement / 15 + 204 + complexity / 15
+        &&  ss->staticEval >= beta - 20 * depth - improvement / 15 + 204 + complexity / 10
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
