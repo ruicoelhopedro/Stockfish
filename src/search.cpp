35f9667a0e1126794379a5d0b2c4455f7ea71273
Rui Coelho <ruicoelhopedro@gmail.com>
2022-01-18 12:34:50 +0000

    Tweak optimism with complexity. take 1 Bench: 5019993

--- src/search.cpp
+++ src/search.cpp
@@ -387,7 +387,8 @@ void Thread::search() {
               trend = (us == WHITE ?  make_score(tr, tr / 2)
                                    : -make_score(tr, tr / 2));
 
-              int opt = sigmoid(prev, 0, 25, 147, 14464, 256);
+              int complexPos = (complexityAverage.value() - 232) / 100;
+              int opt = sigmoid(prev + complexPos, 0, 25, 147, 14464, 256);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }
