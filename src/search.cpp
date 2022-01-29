35c7bb730848e09b9176df3d1c17dc237661eadd
Rui Coelho <ruicoelhopedro@gmail.com>
2022-01-29 19:27:51 +0000

    Adjust qsearch SEE threshold, take 3 Bench: 4581547

--- src/search.cpp
+++ src/search.cpp
@@ -1521,7 +1521,7 @@ moves_loop: // When in check, search starts here
 
       // Do not search moves with negative SEE values (~5 Elo)
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && !pos.see_ge(move, Value(-50)))
+          && !pos.see_ge(move, Value(-25)))
           continue;
 
       // Speculative prefetch as early as possible
