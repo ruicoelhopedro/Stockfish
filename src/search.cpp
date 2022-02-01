4d2f03d092446ec922e9fefe6b5f2f04a78c4c2b
Rui Coelho <ruicoelhopedro@gmail.com>
2022-02-01 14:12:36 +0000

    Disable TT cutoff after null move: search + qsearch Bench: 4348040

--- src/search.cpp
+++ src/search.cpp
@@ -1418,6 +1418,7 @@ moves_loop: // When in check, search starts here
 
     if (  !PvNode
         && ss->ttHit
+        && (ss-1)->currentMove != MOVE_NULL
         && tte->depth() >= ttDepth
         && ttValue != VALUE_NONE // Only in case of TT access race
         && (ttValue >= beta ? (tte->bound() & BOUND_LOWER)
