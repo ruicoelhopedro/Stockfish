d7491a6dac2e06c1a55536c8ae9db583226cab05
Rui Coelho <ruicoelhopedro@gmail.com>
2022-02-03 19:10:06 +0000

    Take 1 Bench: 4329762

--- src/search.cpp
+++ src/search.cpp
@@ -1155,6 +1155,10 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
+          if (   type_of(move) == PROMOTION
+              && complexity > 300)
+              r--;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
