5797d1152c5c1d3b4705ce3fd42fcf5282222b28
Rui Coelho <ruicoelhopedro@gmail.com>
2022-02-03 09:50:45 +0000

    No multi-cut for PV nodes Bench: 4310593

--- src/search.cpp
+++ src/search.cpp
@@ -1079,7 +1079,7 @@ moves_loop: // When in check, search starts here
               // search without the ttMove. So we assume this expected Cut-node is not singular,
               // that multiple moves fail high, and we can prune the whole subtree by returning
               // a soft bound.
-              else if (singularBeta >= beta)
+              else if (!PvNode && singularBeta >= beta)
                   return singularBeta;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
