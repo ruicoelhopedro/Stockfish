8f4fd5e4b01b4c5a071346e0cee5e2b89fa180ba
Rui Coelho <ruicoelhopedro@gmail.com>
2022-02-07 11:16:42 +0000

    No futility pruning after a null move Bench: 4476119

--- src/search.cpp
+++ src/search.cpp
@@ -787,6 +787,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
+        &&  (ss-1)->currentMove != MOVE_NULL
         &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
         &&  eval >= beta
         &&  eval < 15000) // 50% larger than VALUE_KNOWN_WIN, but smaller than TB wins.
