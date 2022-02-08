9ff6982bf0ba34bfb3babac1fe4afff9f9a96516
Rui Coelho <ruicoelhopedro@gmail.com>
2022-02-08 13:59:30 +0000

    LMR reduction on average complexity, take 1 Bench: 4692260

--- src/search.cpp
+++ src/search.cpp
@@ -1167,6 +1167,9 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
+          if (complexity > 2 * thisThread->complexityAverage.value())
+              r--;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
