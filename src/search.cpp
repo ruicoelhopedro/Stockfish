4771c69cbe92a9c175aa8540f04f45f3d47b9489
Rui Coelho <ruicoelhopedro@gmail.com>
2022-03-28 16:55:53 +0100

    Quadratic futility margin Bench: 6910495

--- src/search.cpp
+++ src/search.cpp
@@ -63,7 +63,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(168 * (d - improving));
+    return Value(100 * (d - improving) + 16 * d * d);
   }
 
   // Reductions lookup table, initialized at startup
