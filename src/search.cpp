b7d1ae0a3fac9805361996abc2e20cb744083943
Rui Coelho <ruicoelhopedro@gmail.com>
2022-04-07 14:30:41 +0100

    Less multiCut at high depths Bench: 6665056

--- src/search.cpp
+++ src/search.cpp
@@ -1091,7 +1091,7 @@ moves_loop: // When in check, search starts here
               // search without the ttMove. So we assume this expected Cut-node is not singular,
               // that multiple moves fail high, and we can prune the whole subtree by returning
               // a soft bound.
-              else if (singularBeta >= beta)
+              else if (singularBeta >= beta && depth < 14)
                   return singularBeta;
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
