0d357f56b18cf668951fa14829d8df66072dedf3
Rui Coelho <ruicoelhopedro@gmail.com>
2022-04-16 14:32:51 +0100

    Don't trust cached eval at moderate depth Bench: 5974737

--- src/search.cpp
+++ src/search.cpp
@@ -733,7 +733,7 @@ namespace {
     {
         // Never assume anything about values stored in TT
         ss->staticEval = eval = tte->eval();
-        if (eval == VALUE_NONE)
+        if (eval == VALUE_NONE || depth > 7)
             ss->staticEval = eval = evaluate(pos);
 
         // Randomize draw evaluation
