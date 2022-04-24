cb52f23fcffb5be16ee820de43637b2faed4bbfb
Rui Coelho <ruicoelhopedro@gmail.com>
2022-04-24 01:25:48 +0100

    Less history reductions for PvNodes, take 3 Bench: 7883214

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts here
                          - 4334;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= ss->statScore / (15914 + 5000 * PvNode);
+          r -= ss->statScore / (15914 + 7500 * PvNode);
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
