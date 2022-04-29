eab2b722713e356b672a6f2941c1cb4d6702b510
Rui Coelho <ruicoelhopedro@gmail.com>
2022-04-29 11:31:16 +0100

    Less shalow depth pruning for complex positions, take 2 Bench: 7284161

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta) + (complexity > 500), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta) + (complexity > 750), 0);
 
           if (   capture
               || givesCheck)
