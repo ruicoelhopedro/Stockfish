a9fbed1a15da672464b9e06cc2e244a29c1867aa
Rui Coelho <ruicoelhopedro@gmail.com>
2022-05-20 01:20:40 +0100

    No razoring in singular seaches Bench: 5995838

--- src/search.cpp
+++ src/search.cpp
@@ -782,6 +782,7 @@ namespace {
     // return a fail low.
     if (   !PvNode
         && depth <= 7
+        && !excludedMove
         && eval < alpha - 348 - 258 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
