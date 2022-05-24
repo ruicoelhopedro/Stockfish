119c317246aa009b03b498ef6bd447c9b9b4bdcf
Rui Coelho <ruicoelhopedro@gmail.com>
2022-05-24 17:05:50 +0100

    Less reductions for our opponent responses Bench: 6580772

--- src/search.cpp
+++ src/search.cpp
@@ -1183,6 +1183,9 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if (PvNode && ss->ply == 1)
+              r--;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
