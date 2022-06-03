9c2e623bbcc96538db36430f414662339b41668b
Rui Coelho <ruicoelhopedro@gmail.com>
2022-06-03 16:48:20 +0100

    Use likelyFailLow in quiet ttMove extensions, reverse Bench: 6916659

--- src/search.cpp
+++ src/search.cpp
@@ -1112,7 +1112,7 @@ moves_loop: // When in check, search starts here
 
           // Quiet ttMove extensions (~0 Elo)
           else if (   PvNode
-                   && !likelyFailLow
+                   && likelyFailLow
                    && move == ttMove
                    && move == ss->killers[0]
                    && (*contHist[0])[movedPiece][to_sq(move)] >= 5491)
