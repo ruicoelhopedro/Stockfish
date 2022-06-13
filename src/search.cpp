9b1b179ad86a97eed251468eda3d080c01aa2cb7
Rui Coelho <ruicoelhopedro@gmail.com>
2022-06-13 14:34:22 +0100

    Allow more TT cutoffs when entry has been on the PV Bench: 5377676

--- src/search.cpp
+++ src/search.cpp
@@ -635,7 +635,7 @@ namespace {
     // At non-PV nodes we check for an early TT cutoff
     if (  !PvNode
         && ss->ttHit
-        && tte->depth() > depth - (thisThread->id() % 2 == 1)
+        && tte->depth() > depth - (thisThread->id() % 2 == 1) - ss->ttPv
         && ttValue != VALUE_NONE // Possible in case of TT access race
         && (ttValue >= beta ? (tte->bound() & BOUND_LOWER)
                             : (tte->bound() & BOUND_UPPER)))
