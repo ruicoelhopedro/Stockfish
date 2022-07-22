ba2f38caeba6c39adeb4f58565c7869f2235ba8f
Rui Coelho <ruicoelhopedro@gmail.com>
2022-07-23 00:40:44 +0100

    More reductions for nodes likely to raise alpha, take 2 Bench: 5266568

--- src/search.cpp
+++ src/search.cpp
@@ -1176,7 +1176,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (likelyRaiseAlpha)
+          if (bestMove && likelyRaiseAlpha)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
