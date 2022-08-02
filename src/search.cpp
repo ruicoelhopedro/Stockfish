6f7a53ddc5ff7a55286c1aac73cdaeb047dca25a
Rui Coelho <ruicoelhopedro@gmail.com>
2022-08-02 19:46:38 +0100

    More reductions for early Pv nodes without a best move after some moves Bench: 5493907

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (PvNode && !bestMove && ss->ply <= 3)
+          if (PvNode && !bestMove && ss->ply <= 3 && moveCount > 10)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
