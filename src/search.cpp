c4d5933f6ebed9dac51f3330979f5b3c840c7a95
Rui Coelho <ruicoelhopedro@gmail.com>
2022-08-12 11:34:49 +0100

    Test SS cutNode flag, take 3 Bench: 5509789

--- src/search.cpp
+++ src/search.cpp
@@ -1069,7 +1069,7 @@ moves_loop: // When in check, search starts here
               Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
-              value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, true);
+              value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, !ttCapture);
               ss->excludedMove = MOVE_NONE;
 
               if (value < singularBeta)
