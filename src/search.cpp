8077ead7ad0369bcdae3df180ab9c6f29e199be2
Rui Coelho <ruicoelhopedro@gmail.com>
2022-09-02 15:21:05 +0100

    Less LMR at low depth for PV nodes, take 4 Bench: 6016096

--- src/search.cpp
+++ src/search.cpp
@@ -1131,7 +1131,7 @@ moves_loop: // When in check, search starts here
       // We use various heuristics for the sons of a node after the first son has
       // been searched. In general we would like to reduce them, but there are many
       // cases where we extend a son if it has good chances to be "interesting".
-      if (    depth >= 2 + 3 * PvNode
+      if (    depth >= 2 + (PvNode && improving)
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
