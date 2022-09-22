d0957698b08c1ab2e857bac3686a047ee7342cba
Rui Coelho <ruicoelhopedro@gmail.com>
2022-09-22 10:49:06 +0100

    Increase complexity when ttValue is used for eval, take 1 Bench: 4518581

--- src/search.cpp
+++ src/search.cpp
@@ -743,7 +743,10 @@ namespace {
         // ttValue can be used as a better position evaluation (~4 Elo)
         if (    ttValue != VALUE_NONE
             && (tte->bound() & (ttValue > eval ? BOUND_LOWER : BOUND_UPPER)))
+        {
             eval = ttValue;
+            complexity += abs(eval - ss->staticEval);
+        }
     }
     else
     {
