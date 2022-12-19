7be1e72f862cc01484d5820637e7a9d5d66745a3
Rui Coelho <ruicoelhopedro@gmail.com>
2022-12-19 18:00:04 +0000

    Adjust negative extension, take 1 Bench: 4703679

--- src/search.cpp
+++ src/search.cpp
@@ -1082,7 +1082,7 @@ moves_loop: // When in check, search starts here
 
               // If the eval of ttMove is greater than beta, we reduce it (negative extension)
               else if (ttValue >= beta)
-                  extension = -2;
+                  depth -= 2;
 
               // If the eval of ttMove is less than alpha and value, we reduce it (negative extension)
               else if (ttValue <= alpha && ttValue <= value)
